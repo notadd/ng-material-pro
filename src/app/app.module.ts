eb8a611ef8f13fb61af7dfa2889250330687a09a
小黄 <wish_cheng@163.com>
2018-12-23 17:59:51 +0800

    feat(app): Implement a simple demo page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatIconModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule, MatListModule, MatButtonModule } from '@angular/material';
 
 import { NmAlertModule, NmCarouselModule, NmCascadeDropdownlistModule } from '../../projects/notadd/ng-material2/src/public_api';
 
@@ -16,7 +17,11 @@ import { AppComponent } from './app.component';
         BrowserModule,
         BrowserAnimationsModule,
 
+        FlexLayoutModule,
+
         MatIconModule,
+        MatListModule,
+        MatButtonModule,
 
         NmAlertModule,
         NmCarouselModule,
